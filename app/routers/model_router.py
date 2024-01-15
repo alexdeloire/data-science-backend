e9e200a26bc36eeab45729b4210f8de0a6cd8e64
alexandre <alexandre.deloire01@etu.umontpellier.fr>
2024-01-15 18:52:31 +0100

    all models

--- app/routers/model_router.py
+++ app/routers/model_router.py
@@ -2,6 +2,23 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
 from pydantic import BaseModel
 from ..controllers.model_controller import predict_sector
+from app.controllers.globals import (
+    get_tokenizer,
+    get_encoder,
+    get_model,
+    get_encoderPolyChatA,
+    get_encoderPolyChatI,
+    get_encoderPolyChatR,
+    get_encoderPolyChatU,
+    get_modelPolyChatA,
+    get_modelPolyChatI,
+    get_modelPolyChatR,
+    get_modelPolyChatU,
+    get_tokenizerPolyChatA,
+    get_tokenizerPolyChatI,
+    get_tokenizerPolyChatR,
+    get_tokenizerPolyChatU
+)
 
 model_router = APIRouter(
     tags=["formations"],
@@ -19,3 +36,44 @@ def predict_sector_from_query(query: Query, request: Request):
     except HTTPException as e:
         raise e
 
+# Enseignements absents qui auraient ete utiles
+@model_router.post("/predict_sectorPolyChatA/")
+def predict_sector_from_queryPolyChatA(query: Query, request: Request):
+    try:
+        query = query.query
+        predicted_sector = predict_sector(query, get_tokenizerPolyChatA(), get_encoderPolyChatA(), get_modelPolyChatA())
+        return {"predicted_sector": predicted_sector}
+    except HTTPException as e:
+        raise e
+    
+# Enseignements presents inutiles
+@model_router.post("/predict_sectorPolyChatI/")
+def predict_sector_from_queryPolyChatI(query: Query, request: Request):
+    try:
+        query = query.query
+        predicted_sector = predict_sector(query, get_tokenizerPolyChatI(), get_encoderPolyChatI(), get_modelPolyChatI())
+        return {"predicted_sector": predicted_sector}
+    except HTTPException as e:
+        raise e
+    
+# Enseignements presents a renforcer
+@model_router.post("/predict_sectorPolyChatR/")
+def predict_sector_from_queryPolyChatR(query: Query, request: Request):
+    try:
+        query = query.query
+        predicted_sector = predict_sector(query, get_tokenizerPolyChatR(), get_encoderPolyChatR(), get_modelPolyChatR())
+        return {"predicted_sector": predicted_sector}
+    except HTTPException as e:
+        raise e
+
+# Enseignements presents utiles
+@model_router.post("/predict_sectorPolyChatU/")
+def predict_sector_from_queryPolyChatU(query: Query, request: Request):
+    try:
+        query = query.query
+        predicted_sector = predict_sector(query, get_tokenizerPolyChatU(), get_encoderPolyChatU(), get_modelPolyChatU())
+        return {"predicted_sector": predicted_sector}
+    except HTTPException as e:
+        raise e
+    
+    
