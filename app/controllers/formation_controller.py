9153ae760c05af9f33172f31717eaa844b260321
Jiayi HE <90317106+JiayiHE95@users.noreply.github.com>
2024-01-16 22:23:55 +0100

    Update formation_controller.py

--- app/controllers/formation_controller.py
+++ app/controllers/formation_controller.py
@@ -16,6 +16,91 @@ def read_excel(file_path, column_name):
         raise HTTPException(status_code=500, detail=f"Error reading Excel file: {str(e)}")
 
 
+def count_response(file_path, column):
+    try:
+        df = pd.read_excel(file_path)
+        formations = df[column].tolist()
+        count = len(formations)
+        return count
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error reading Excel file: {str(e)}")
+    
+
+def count_sexe_by_formation(file_path,column_name):
+    try:
+        df = pd.read_excel(file_path)
+        formations = df["formation"].tolist()
+        sexe = df[column_name].tolist()
+        result_dict = {}
+
+        for form, sex in zip(formations, sexe):
+            if form not in result_dict:
+                result_dict[form] = {}
+            if sex not in result_dict[form]:
+                result_dict[form][sex] = 1
+            else:
+                result_dict[form][sex] += 1
+                
+        return result_dict
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error reading Excel file: {str(e)}")
+
+
+def count_by_formation(file_path, column_name):
+    try:
+        df = pd.read_excel(file_path)
+        formations = df["formation"].tolist()
+        #print("Noms de colonnes disponibles dans le DataFrame:\n", df.columns.tolist())
+        #print("\n\n", df[column_name])
+        
+        # Convertir les valeurs de satisfaction en chaînes de caractères
+        satisfaction = df[column_name].astype(str).tolist()
+        
+        result_dict = {}
+
+        for form, sat in zip(formations, satisfaction):
+            if form not in result_dict:
+                result_dict[form] = {}
+            
+            # Vérifier si la valeur de satisfaction n'est pas NaN
+            if sat != 'nan':
+                if sat not in result_dict[form]:
+                    result_dict[form][sat] = 1
+                else:
+                    result_dict[form][sat] += 1
+                
+        return result_dict
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error reading Excel file: {str(e)}")
+    
+
+def count_by_text_response(file_path, question):
+    try:
+        df = pd.read_excel(file_path)
+        formations = df["formation"].tolist()
+        responses = df[question].tolist()
+        
+        result_dict = {}
+
+        for form, resp in zip(formations, responses):
+            # Vérifier si la valeur de la réponse n'est pas NaN et est une chaîne de caractères non vide
+            if not pd.isna(resp) and isinstance(resp, str) and resp.strip():
+                key = "true"
+            else:
+                key = "false"
+            
+            result_dict.setdefault(form, { "true": 0, "false": 0 })
+            result_dict[form][key] += 1
+
+        return result_dict
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error reading Excel file: {str(e)}")
+
+    
+    
 if __name__ == "__main__":
     print(read_excel("formatted_data/2023.xlsx", "formation"))
     
