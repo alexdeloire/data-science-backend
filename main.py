e28ff8a04e9d065a79e5cc8dbcc0310aec250d80
alexandre <alexandre.deloire01@etu.umontpellier.fr>
2024-01-17 19:44:43 +0100

    cors

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://poly-analyse.onrender.com","http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
