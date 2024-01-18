7ad247ff988c647b9a3e8c785a00a0caaf856cf7
remi <remijorge5@gmail.com>
2024-01-18 17:17:44 +0100

    kmeans routes added

--- app/routers/kmeans_router.py
+++ app/routers/kmeans_router.py
@@ -1,6 +1,8 @@
 # routers/kmeans_router.py
 from fastapi import APIRouter, Depends, HTTPException
 from ..controllers.kmeans_controller import get_useful_lessons
+from ..controllers.kmeans_controller import get_absent_lessons
+from ..controllers.kmeans_controller import get_advice
 
 kmeans_router = APIRouter(
     tags=["kmeans"],
@@ -8,7 +10,7 @@ kmeans_router = APIRouter(
 
 
 @kmeans_router.get("/kmeans/useful-lessons")
-def predict_sector_from_query(formation: str = None):
+def predict_sector_from_query_useful(formation: str = None):
     try:
         if formation is None : 
             formation = "ALL"
@@ -17,3 +19,22 @@ def predict_sector_from_query(formation: str = None):
     except HTTPException as e:
         raise e
 
+@kmeans_router.get("/kmeans/absent-lessons")
+def predict_sector_from_query_absent(formation: str = None):
+    try:
+        if formation is None : 
+            formation = "ALL"
+        file = get_absent_lessons(formation)
+        return file
+    except HTTPException as e:
+        raise e
+    
+@kmeans_router.get("/kmeans/advice")
+def predict_sector_from_query_advice(formation: str = None):
+    try:
+        if formation is None : 
+            formation = "ALL"
+        file = get_advice(formation)
+        return file
+    except HTTPException as e:
+        raise e
